0c903dd681c57813045c692bd2f6c85254b6c01b
anmol sharma <anmolsharma433@gmail.com>
2019-12-04 23:23:43 -0500

    Customer Data Columns added in Customer class

--- app/src/main/java/com/example/employeepayrollsystem/RoomDatabase/Customer.java
+++ app/src/main/java/com/example/employeepayrollsystem/RoomDatabase/Customer.java
@@ -1,4 +1,77 @@
 package com.example.employeepayrollsystem.RoomDatabase;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "Customer_Info")
 public class Customer {
+    @PrimaryKey(autoGenerate = true)
+    private int cid;
+
+    @ColumnInfo(name = "FirstName")
+    private String firstname;
+
+    @ColumnInfo(name = "LastName")
+    private String lastname;
+
+    @ColumnInfo(name = "Email_ID")
+    private String emailId;
+
+    @ColumnInfo(name = "Password")
+    private String password;
+
+    @ColumnInfo(name = "Date_Of_Birth")
+    private String dob;
+
+
+    //Getter and Setter added
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
 }
--- app/src/main/java/com/example/employeepayrollsystem/RoomDatabase/UserDatabase.java
+++ app/src/main/java/com/example/employeepayrollsystem/RoomDatabase/UserDatabase.java
@@ -1,6 +1,26 @@
 package com.example.employeepayrollsystem.RoomDatabase;
 
+import android.content.Context;
+
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.Room
 
-@Database(entities = )
+@Database(entities = Customer.class,exportSchema = false,version = 2)
+public abstract class UserDatabase extends RoomDatabase{
+    public static final String Database_name = "Employee_Payroll";
+    private static  UserDatabase uInstance;
+
+
+    public static UserDatabase getInstance(Context context)
+    {
+        if(uInstance == null)
+        {
+            uInstance = Room.databaseBuilder(context.getApplicationContext(),UserDatabase.class,UserDatabase.Database_name).allowMainThreadQueries().fallbackToDestructiveMigration().build();
+        }
+        return uInstance;
+    }
+
+}
+
+
