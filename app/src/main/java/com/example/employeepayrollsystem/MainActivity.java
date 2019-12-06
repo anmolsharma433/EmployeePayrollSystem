4caadcfff758aa1d20210ae9d7bcaac6e160fa2d
Ritik Jagpal <55303144+ritjagpal26@users.noreply.github.com>
2019-12-06 18:15:53 -0500

    hey

--- app/src/main/java/com/example/employeepayrollsystem/MainActivity.java
+++ app/src/main/java/com/example/employeepayrollsystem/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.employeepayrollsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button1);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Employee_rv.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
+
+
 }
